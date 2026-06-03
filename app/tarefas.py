ddcc6fb06eff74a4cd4c92e30b6b42037b9a07bd
Renata Bezerra <renatabezerra0208@gmail.com>
2026-06-03 14:54:35 -0300

    Base de tarefas

---
+++ app/tarefas.py
@@ -0,0 +1,16 @@
+## GERENCIAMENTO DE TAREFAS
+
+def cadastrar_tarefa():
+    pass
+
+def listar_tarefas_por_evento(evento_id):
+    pass
+
+def editar_tarefa():
+    pass
+
+def alterar_status_tarefa():
+    pass
+
+def excluir_tarefa():
+    pass
